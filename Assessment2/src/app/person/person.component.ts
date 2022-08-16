ebd476651920061b15267683ede724002dec82d7
shantanu057 <shantanu057@gmail.com>
2022-08-16 13:23:45 +0530

    committed

--- Assessment2/src/app/person/person.component.ts
+++ Assessment2/src/app/person/person.component.ts
@@ -8,7 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class PersonComponent {
 
-  iserror:boolean=false;
+  // iserror:boolean=false;
+  // selected = 'option2';
 
   gen: any[] = [
     {value: 'Male', viewValue: 'Male'},
